720b868d2e0f16f92ab8ce7dcbf454ed724b2db6
jcr04 <joaocarlosreboucas15@gmail.com>
2023-12-10 19:12:40 -0300

    add news routes

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,24 +4,19 @@ import (
 	"github.com/spf13/viper"
 )
 
-// Config representa as configurações da aplicação.
 type Config struct {
 	DatabaseURL string
 }
 
-// LoadConfig carrega as configurações da aplicação.
 func LoadConfig() (*Config, error) {
 	viper.SetConfigName("config") // Nome do arquivo de configuração (sem a extensão)
 	viper.SetConfigType("yaml")   // Extensão do arquivo de configuração
 	viper.AddConfigPath(".")      // Procura o arquivo de configuração no diretório atual
 
-	// Configuração padrão
 	viper.SetDefault("DatabaseURL", "postgres://postgres:12345@localhost:5432/Plataform")
 
-	// Sobrescreve com variáveis de ambiente, se existirem
 	viper.AutomaticEnv()
 
-	// Lê o arquivo de configuração
 	err := viper.ReadInConfig()
 	if err != nil {
 		return nil, err
--- pkg/gateway/router.go
+++ pkg/gateway/router.go
@@ -1,13 +1,30 @@
 package gateway
 
 import (
+	"log"
+	"net/http/httputil"
+	"net/url"
+
 	"github.com/gorilla/mux"
 	"github.com/jcr04/Plataform-API.go/pkg/middleware"
 )
 
-// NewRouter cria e retorna um novo roteador com as rotas configuradas.
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
+
+	auauPetsURL, err := url.Parse("http://localhost:8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auauPetsProxy := httputil.NewSingleHostReverseProxy(auauPetsURL)
+
+	router.Handle("/animals", auauPetsProxy)
+	router.Handle("/animals/{id}", auauPetsProxy)
+	router.Handle("/reservations", auauPetsProxy)
+	router.Handle("/reservations/{id}", auauPetsProxy)
+	router.Handle("/hostings", auauPetsProxy)
+	router.Handle("/hostings/{id}", auauPetsProxy)
 
 	protectedRoutes := router.PathPrefix("/protected").Subrouter()
 	protectedRoutes.Use(middleware.AuthMiddleware)
